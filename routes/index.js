d52c1eb1b306a3b26eeb459f2531ab04718685fa
Qin Cheng <pqc92@hotmail.com>
2016-06-09 01:17:20 +0800

    removed empty line

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,6 @@ router.post('/object', function(req, res, next){
   });
 });
 
-
 router.get('/', function(req, res, next) {
   var result = req.query.result || '';
   res.render('index', { result: result });
